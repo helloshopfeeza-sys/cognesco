1dada6865e23d68476971693b8b22a9c2f5dcf86
Mohamed Fakhar <mohamedfakhar@Mohameds-MacBook-Air.local>
2026-02-23 17:45:50 +0530

    chore: setup vite-plugin-sitemap and robots.txt

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
+
+const dynamicRoutes = [
+  '/',
+  '/about',
+  '/services',
+  '/work',
+  '/process',
+  '/faq',
+  '/contact',
+  '/services/web-development',
+  '/services/mobile-applications',
+  '/services/ecommerce-web',
+  '/services/ecommerce-app',
+  '/services/ui-ux-design'
+]
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://cognescosolutions.com',
+      dynamicRoutes,
+      // Optional: you can explicitly define static routes if they are not picked up automatically
+      // readableRoutes: ['/', '/about', '/services', '/work', '/process', '/faq', '/contact']
+    })
+  ],
 })
